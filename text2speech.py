3d38d60a2f0f3bccc7a1e46ba583792fd304faff
cbk914 <cbk914@riseup.net>
2023-06-11 03:19:37 +0200

    Update text2speech.py

--- text2speech.py
+++ text2speech.py
@@ -1,157 +1,161 @@
+import argparse
+import json
 import os
-import re
+from datetime import datetime
+from functools import partial
+from typing import List
+
+import pathlib
 from dotenv import load_dotenv, set_key
 from google.cloud import texttospeech
 from termcolor import colored
-from typing import List
-import pathlib
 import glob
-import nltk
+from langdetect import detect
 
 load_dotenv()
 
-GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+def str_lower(s):
+    return s.lower()
 
-if GOOGLE_APPLICATION_CREDENTIALS is None or not os.path.isfile(GOOGLE_APPLICATION_CREDENTIALS):
-    print(colored("Google Cloud credentials file path not found.", "red"))
+# variable to store the "all" choice
+replace_all = None
+
+parser = argparse.ArgumentParser(description='Convert text files to speech.')
+parser.add_argument('-l', '--language', type=str, help='The language code.')
+parser.add_argument('-v', '--voice', type=str, help='The name or number of the voice.')
+parser.add_argument('-f', '--format', type=str_lower, choices=['mp3', 'wav', 'ogg'], help='The audio output format.')
+parser.add_argument('-p', '--path', type=str, default='./', help='The path to the input file (use wildcard * for multiple files).')
+parser.add_argument('-s', '--save', type=str, help='Save current settings to a profile.')
+parser.add_argument('-r', '--load', type=str, help='Load settings from a profile.')
+args = parser.parse_args()
+
+config = {}
+if args.load:
+    with open(args.load, 'r') as f:
+        config = json.load(f)
+    args.language = config.get('language', args.language)
+    args.voice = config.get('voice', args.voice)
+    args.format = config.get('format', args.format)
+    args.path = config.get('path', args.path)
+
+# Prompt the user for Google credentials if not found
+def get_credentials():
     while True:
-        GOOGLE_APPLICATION_CREDENTIALS = input(colored("Please enter the path to your Google Cloud credentials file: ", "blue"))
-        if os.path.isfile(GOOGLE_APPLICATION_CREDENTIALS):
-            break
-        else:
-            print(colored("Invalid file path. Please try again.", "red"))
-    set_key(".env", "GOOGLE_APPLICATION_CREDENTIALS", GOOGLE_APPLICATION_CREDENTIALS)
+        path = input(colored("Please enter the path to your Google Cloud credentials file: ", "blue"))
+        if os.path.isfile(path):
+            return path
+        print(colored("Invalid file path. Please try again.", "red"))
 
+# Load Google credentials
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS") or get_credentials()
+set_key(".env", "GOOGLE_APPLICATION_CREDENTIALS", GOOGLE_APPLICATION_CREDENTIALS)
 client = texttospeech.TextToSpeechClient.from_service_account_json(GOOGLE_APPLICATION_CREDENTIALS)
 
-def get_files():
-    while True:
-        file_path_input = input(colored("Enter the path to the input file (use wildcard * for multiple files): ", "blue"))
-        file_paths = glob.glob(file_path_input)  # Handle wildcards in file paths
-        if file_paths:
-            return file_paths
-        else:
-            print(colored("No files found. Please try again.", "red"))
+# mapping between ISO 639-1 codes to BCP-47 codes.
+iso_to_bcp = {
+    'zh': 'zh-CN',  # Mandarin
+    'es': 'es-ES',  # Spanish
+    'en': 'en-US',  # English
+    'hi': 'hi-IN',  # Hindi
+    'bn': 'bn-IN',  # Bengali
+    'pt': 'pt-BR',  # Portuguese (Brazilian variant)
+    'ru': 'ru-RU',  # Russian
+    'ja': 'ja-JP',  # Japanese
+    'pa': 'pa-Guru-IN',  # Punjabi (Gurmukhi script, Indian variant)
+    'mr': 'mr-IN'  # Marathi
+    # add more language mappings here    
+}
+
+def prompt_for_file():
+    user_input = input("Please enter a file path or 'q' to quit: ")
+    if user_input.lower() == 'q':
+        exit(0)
+    elif os.path.isfile(user_input):
+        return [user_input]
+    else:
+        print(f"No file found for the path: {user_input}")
+        return prompt_for_file()
 
 def list_voices() -> None:
-    voices = client.list_voices().voices
-    return voices
+    response = client.list_voices()
+    for voice in response.voices:
+        print(voice)
 
-def filter_voices_by_language(voices, language_code):
-    filtered_voices = [voice for voice in voices if language_code in voice.language_codes]
-    return filtered_voices
+def get_voices(language_code):
+    response = client.list_voices(language_code)
+    return response.voices
 
-def print_voices(voices):
-    voice_dict = {i + 1: voice for i, voice in enumerate(voices)}
-    for num, voice in voice_dict.items():
-        print(colored(f"{num}. Name: {voice.name}, Gender: {voice.ssml_gender}, Natural Sample Rate Hertz: {voice.natural_sample_rate_hertz}", "green"))
-    return voice_dict
-
-def get_text_and_voice() -> tuple:
-    default_language_code = os.getenv('LANGUAGE_CODE', 'en-US')
-    language_code = input(colored(f"Enter the language code (default: {default_language_code}): ", "blue")) or default_language_code
-    set_key('.env', 'LANGUAGE_CODE', language_code)
-
-    voices = list_voices()
-    filtered_voices = filter_voices_by_language(voices, language_code)
-    print(colored(f"Available voices for language {language_code}:", "green"))
-    voice_dict = print_voices(filtered_voices)
-
-    default_voice_name = os.getenv('VOICE_NAME', list(voice_dict.values())[0].name)
-
+def get_voice_choice(voice):
+    for i, v in enumerate(get_voices(language_code), 1):
+        print(f"{i}. {v.name}")
+        print(f"\tLanguage: {v.language_codes[0]}")
+        print(f"\tGender: {texttospeech.SsmlVoiceGender(v.ssml_gender).name}")
     while True:
-        voice_input = input(colored(f"Enter the number or name of the voice you want to use (default: {default_voice_name}): ", "blue")) or default_voice_name
-        try:
-            if voice_input.isdigit():
-                voice_name = voice_dict[int(voice_input)].name
-                ssml_gender = voice_dict[int(voice_input)].ssml_gender.name  # Use .name to get the name of the enum value
-            else:
-                voice_name = voice_input
-                ssml_gender = next((v.ssml_gender.name for v in voice_dict.values() if v.name == voice_input), None) 
-            if ssml_gender is None:
-                raise KeyError()
-            break
-        except KeyError:
-            print(colored("Invalid selection. Please try again.", "red"))
-    
-    set_key('.env', 'VOICE_NAME', voice_name)
-    set_key('.env', 'SSML_GENDER', ssml_gender)
+        user_input = input("Please enter a number or 'q' to quit: ")
+        if user_input.lower() == 'q':
+            exit(0)
+        elif user_input.isdigit() and 0 < int(user_input) <= len(get_voices(language_code)):
+            return get_voices(language_code)[int(user_input)-1].name
 
-    return language_code, texttospeech.SsmlVoiceGender[ssml_gender], voice_name  # Convert back to enum for use with the API
+def get_language_code(text):
+    iso_code = detect(text)
+    bcp_code = iso_to_bcp.get(iso_code, 'en-US')  # default to English if not found
+    return bcp_code
 
-def process_file_input(path: str, max_length: int = 5000) -> List[str]:
-    # Open the file and read the content
-    with open(path, "r") as f:
-        content = f.read()
+def str_lower(s):
+    return s.lower()
 
-    # Split the text based on timestamps
-    segments = re.split(r'(\[\d{2}:\d{2}:\d{2}\])', content)
-    text_chunks = []
+def process_file_input(file_path):
+    file = open(file_path, 'r')
+    file_output = []
+    for line in file.readlines():
+        if len(line.strip()) > 0:
+            file_output.append(line.strip())
+    file.close()
+    return file_output
 
-    # Process each segment
-    for segment in segments:
-        # Further split large segments into smaller chunks
-        if len(segment) > max_length:
-            sentences = nltk.tokenize.sent_tokenize(segment)
-            chunk = ""
-            for sentence in sentences:
-                if len(chunk) + len(sentence) <= max_length:
-                    chunk += " " + sentence
-                else:
-                    text_chunks.append(chunk.strip())
-                    chunk = sentence
-            if chunk:
-                text_chunks.append(chunk)
-        else:
-            text_chunks.append(segment)
-    
-    return text_chunks
+def process_output(output, input_file):
+    date = datetime.today().strftime('%Y-%m-%d')
+    file_name = os.path.splitext(os.path.basename(input_file))[0] + "_" + date + "." + args.format
+    with open(file_name, 'wb') as out:
+        out.write(output.audio_content)
+    print(f"Audio content written to file {file_name}")
 
-# Make sure to download the punkt tokenizer
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
+def synthesize_speech(text, language_code, voice_name):
+    input_text = texttospeech.SynthesisInput(text=text)
+    voice_params = texttospeech.VoiceSelectionParams(
+        language_code=language_code,
+        name=voice_name
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=getattr(texttospeech.AudioEncoding, args.format.upper())
+    )
+    response = client.synthesize_speech(input=input_text, voice=voice_params, audio_config=audio_config)
+    return response
 
-def synthesize_speech(text_chunks: List[str], language_code: str, ssml_gender: str, name: str, filename: str, output_format: str) -> None:
-    output_dir = pathlib.Path(filename)
-    output_dir.mkdir(exist_ok=True)
-
-    for i, chunk in enumerate(text_chunks):
-        input_text = texttospeech.SynthesisInput(text=chunk)
-        voice = texttospeech.VoiceSelectionParams(
-            language_code=language_code, ssml_gender=ssml_gender, name=name
-        )
-        audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding[output_format])
-        response = client.synthesize_speech(input=input_text, voice=voice, audio_config=audio_config)
-
-        with open(output_dir / f"{filename}_{i}.{output_format.lower()}", "wb") as out:
-            out.write(response.audio_content)
-
-def get_output_format():
-    default_format = os.getenv('OUTPUT_FORMAT', 'MP3')
-    while True:
-        format_option = input(colored(f"Enter the audio output format (MP3, WAV, OGG, default: {default_format}): ", "blue")).upper() or default_format
-        if format_option in ['MP3', 'WAV', 'OGG']:
-            set_key('.env', 'OUTPUT_FORMAT', format_option)
-            return format_option
-        else:
-            print(colored("Invalid selection. Please try again.", "red"))        
-
-def main():
-    try:
-        language_code, ssml_gender, voice_name = get_text_and_voice()
-        output_format = get_output_format()
-
-        file_paths = get_files()
-        for file_path in file_paths:
-            text_chunks = process_file_input(file_path)
-            file_name = os.path.splitext(os.path.basename(file_path))[0]
-            synthesize_speech(text_chunks, language_code, ssml_gender, voice_name, file_name, output_format)
-
-        print(colored("Speech synthesis completed, check the created audio files.", "green"))
-    except Exception as e:
-        print(colored(f"An error occurred: {e}", "red"))
+def create_audio_files(input_files):
+    for file in input_files:
+        input_text = process_file_input(file)
+        language_code = get_language_code(' '.join(input_text[:10]))
+        voice_name = get_voice_choice(language_code) if args.voice is None else args.voice
+        print(f"Processing file: {file}")
+        for i, line in enumerate(input_text):
+            print(f"Processing line {i+1} of {len(input_text)}")
+            response = synthesize_speech(line, language_code, voice_name)
+            process_output(response, file)
 
 if __name__ == "__main__":
-    main()
+    input_files = glob.glob(args.path) if '*' in args.path else [args.path]
+    if len(input_files) == 0 or not all(map(os.path.isfile, input_files)):
+        input_files = prompt_for_file()
+    create_audio_files(input_files)
+
+    if args.save:
+        config = {
+            'language': args.language,
+            'voice': args.voice,
+            'format': args.format,
+            'path': args.path
+        }
+        with open(args.save, 'w') as f:
+            json.dump(config, f)
